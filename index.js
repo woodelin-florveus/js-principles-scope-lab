9534963b88ba12967d5c837d82f88beed74c8df8
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-02 20:47:21 -0500

    commit tree db412a58dff0368764fed462a62a076f86ac3912

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+var bestCustomer 
+
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase();
+
+}
+
+
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = customerName
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'poor bob'
+}
+
+//  console.log(setBestCustomer());
